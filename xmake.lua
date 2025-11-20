053dfc9e73f78a927be3fac51fc147b6dea67e30
ylf <1183887021@qq.com>
2025-11-20 18:57:50 +0800

    Block

--- xmake.lua
+++ xmake.lua
@@ -46,6 +46,12 @@ target("memtable")
     add_files("src/memtable/*.cpp")
     add_includedirs("include", {public = true})
 
+target("block")
+    set_kind("static")  -- 生成静态库
+    add_deps("config")
+    add_files("src/block/*.cpp")
+    add_packages("toml11", "spdlog")
+    add_includedirs("include", {public = true})
 
 target("test_skiplist")
     set_kind("binary")  -- 生成可执行文件
@@ -60,6 +66,15 @@ target("test_memtable")
     set_group("tests")
     add_files("test/test_memtable.cpp")
     add_deps("logger", "memtable")  -- 如果memtable是独立的target，这里需要添加对应的依赖
+    add_packages("gtest")
+    add_packages("toml11", "spdlog")
+    add_includedirs("include")
+
+target("test_block")
+    set_kind("binary")
+    set_group("tests")
+    add_files("test/test_block.cpp")
+    add_deps("logger", "block")
     add_packages("gtest")
     add_packages("toml11", "spdlog")
     add_includedirs("include")
